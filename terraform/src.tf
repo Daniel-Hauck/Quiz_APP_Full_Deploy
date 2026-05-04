90a5dee5ccb797f9c92874e3dead750a1ab79477
Daniel Hauck <hauck-daniel@web.de>
2026-05-04 20:45:01 +0200

    src.tf

---
+++ terraform/src.tf
@@ -0,0 +1,32 @@
+# ecr.tf
+resource "aws_ecr_repository" "main" {
+  name                 = "quiz-app"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "example" {
+  repository = aws_ecr_repository.main.name
+
+  policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "behalte die neuesten 5 images",
+      "selection": {
+        "tagStatus": "any",
+        "countType": "imageCountMoreThan",
+        "countNumber": 5
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
