1fbfeaa5fd6f077d09e3c2464f4397a8ed6e53a2
Daniel Hauck <hauck-daniel@web.de>
2026-05-06 18:56:32 +0200

    Fix

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,9 +1,9 @@
-output "cr_repository_name" {
-    description = "Name of the ECR repo"
-    value = aws_ecr_repository.main
+output "ecr_repository_name" {
+  description = "Name of the ECR repository for the application"
+  value       = aws_ecr_repository.app_ecr_repo.name
 }
 
 output "ecr_repository_url" {
-    description = "Full URL of the ECR repo"
-    value = aws_ecr_repository.main
+  description = "Full URL of the ECR repository (registry/repo)"
+  value       = aws_ecr_repository.app_ecr_repo.repository_url
 }
